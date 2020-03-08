ac29e0eb705f21cb0ec620b4f3848cd1a5e17267
BuildTools <unconfigured@null.spigotmc.org>
2020-03-08 17:50:46 +0200

    jenkins instance is up

--- terraform/build.sh
+++ terraform/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Script to run terraform
+set -e
+
 Terraform=$( which terraform )
 [ -z $Terraform ] && echo 'Please install terraform and make sure it is in tour $PATH.' && exit 2
 [ "$( terraform --version | head -n 1 | grep -c 'v0.12')" == '0' ] && echo "Please use terraform version 12" && exit 2 
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,24 +1,11 @@
 
-
-data "aws_vpc" "main" {
-  id = aws_vpc.main.id
-}
-
-data "aws_subnet_ids" "infra" {
-  vpc_id = data.aws_vpc.main.id
-
-  tags = {
-    Name = "Infra"
-  }
-}
-
-resource "aws_instance" "Jenkins" {
-//  for_each      = data.aws_subnet_ids.infra.ids
+resource "aws_instance" "jenkins" {
   ami           = var.jenkins_ami
   instance_type = "t2.micro"
-  subnet_id    = element(data.aws_subnet_ids.infra.vpc_id[*], 1)
+  subnet_id    = aws_subnet.infra.id 
   key_name      = var.keypair
   tags = {
-    Name = "jenkins"
+    name = "jenkins"
   }
 }
+
